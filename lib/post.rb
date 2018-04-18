aea1f33cb36c6cb3c1666df1db87de2abf77ee26
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-18 02:25:54 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,12 +1,12 @@
 class Post
   attr_accessor :name, :author, :title
 
-  def initialize(title)
+  def initialize(title, name = nil)
     @title = title
     @name = name
   end
 
   def author_name
     self.author.name
-  end 
+  end
 end
