7ae239001b7ffdc5ad1bddd82ffdf9194c230cd3
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-18 02:22:45 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,15 +15,16 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     add_post(post)
-    @@post_count += 1
+
   end
 
   def self.post_count
     @@post_count
-  end 
+  end
 end
