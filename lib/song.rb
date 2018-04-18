96862630cbf4184757cd191c430a831ce7fb6e58
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-18 02:38:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,14 @@ class Song
 
   def initialize(name)
     @name = name
-    
+
   end
 
   def artist_name
-    if self.artist 
+    if self.artist
     self.artist.name
   else
     nil 
   end
-
+ end
 end
