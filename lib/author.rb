dd543c6f4b397455563ddc69d7ec317591cd8077
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-18 02:12:24 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,8 +19,7 @@ class Author
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    binding.pry 
+    add_post(post)
+    #binding.pry 
   end
 end
